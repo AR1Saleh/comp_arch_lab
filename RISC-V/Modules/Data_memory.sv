4adf9d662341fe2670654f715067889e6e72ad92
Alex <mrsyed212@gmail.com>
2025-03-02 19:30:37 +0500

    The single-cycle processor works for the 1st test.

--- RISC-V/Modules/Data_memory.sv
+++ RISC-V/Modules/Data_memory.sv
@@ -6,18 +6,18 @@ module Data_memory(
     );
 
 int i;
-logic [31:0] data_mem [63:0];
-assign rdata = (rd_en) ? data_mem[addr[5:0]]: 32'b0;
+logic [31:0] data_mem [128:0];
+assign rdata = (rd_en) ? data_mem[addr]: 32'b0;
 
 always @(negedge clk, negedge rst_n) begin
-if (!rst_n) begin
-  for (i=0;i<64;i++) begin
-    data_mem[i] <= 32'b0;
-  end  
-end
-else if (wr_en) begin
-    data_mem[addr] <= wr_data;
-end
+    if (!rst_n) begin
+      for (i=0;i<128;i++) begin
+        data_mem[i] <= 32'b0;
+      end  
+    end
+    else if (wr_en) begin
+      data_mem[addr] <= wr_data;
+    end
 end
 endmodule
 
